0b8331694974094fd8ee9fd1442a8be7143a0b74
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-04-12 10:16:13 +0100

    Partial commit of PR72

--- tests/testthat/test_deriv.R
+++ tests/testthat/test_deriv.R
@@ -43,13 +43,11 @@ test_that("Analytic derivatives match numeric",{
     expect_lt(deriv_error(fitg), err)
     fitg <- flexsurvreg(formula = Surv(ovarian$futime, ovarian$fustat) ~ 1, dist="gompertz")
     expect_lt(deriv_error(fitg), err)
-    suppressWarnings({fitg <- flexsurvreg(formula = Surv(ovarian$futime, ovarian$fustat) ~ 1, dist="gompertz", method="Nelder-Mead")})
-    expect_lt(deriv_error(fitg), err)
     fitg # With derivs, finds wrong MLE if initialize shape at zero, so default to 0.001 instead.
     fitg <- flexsurvreg(formula = Surv(ovarian$futime, ovarian$fustat) ~ factor(ovarian$rx) + factor(ovarian$resid.ds), dist="gompertz")
     expect_lt(deriv_error(fitg), err)
-    ov2 <- ovarian[ovarian$futime>200,]
-    suppressWarnings({fitg <- flexsurvreg(formula = Surv(rep(200,nrow(ov2)), ov2$futime, ov2$fustat) ~ factor(ov2$rx) + factor(ov2$resid.ds), dist="gompertz")}) # truncation
+    ov2 <- ovarian[ovarian$futime>150,]
+    fitg <- flexsurvreg(formula = Surv(rep(150,nrow(ov2)), ov2$futime, ov2$fustat) ~ factor(ov2$rx) + factor(ov2$resid.ds), dist="weibull")# truncation
     expect_lt(deriv_error(fitg), err)
     fitg <- flexsurvreg(formula = Surv(ovarian$futime, ovarian$fustat) ~ shape(factor(ovarian$resid.ds)), dist="gompertz")
     expect_lt(deriv_error(fitg), err)
@@ -59,7 +57,8 @@ test_that("Analytic derivatives match numeric",{
 
     
 ## fixedpars
-    suppressWarnings({fitg <- flexsurvreg(formula = Surv(rep(200,nrow(ov2)), ov2$futime, ov2$fustat) ~ factor(ov2$rx) + factor(ov2$resid.ds), dist="gompertz", fixedpars=2)})
+    ov2 <- ovarian[ovarian$futime>200,]
+    fitg <- flexsurvreg(formula = Surv(rep(200,nrow(ov2)), ov2$futime, ov2$fustat) ~ factor(ov2$rx) + factor(ov2$resid.ds), dist="weibull", fixedpars=2)
     expect_lt(deriv_error(fitg), err)
 
     fitl <- flexsurvreg(formula = Surv(rep(200,nrow(ov2)), ov2$futime, ov2$fustat) ~ factor(ov2$rx) + factor(ov2$resid.ds), dist="llogis", fixedpars=2)
