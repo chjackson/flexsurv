23aa6bef768a68ad3305802d992aa231bd545da2
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2020-05-16 12:50:49 +0100

    Cleaned a bit and updated TODOs.

--- R/predict.flexsurvreg.R
+++ R/predict.flexsurvreg.R
@@ -4,6 +4,13 @@
 ##'
 ##' @inheritParams summary.flexsurvreg
 ##'
+##' @param newdata Data frame containing covariate values to produce fitted
+##' values for, or a list that can be coerced to such a data frame.  There
+##' must be a column for every covariate in the model formula, and one row for
+##' every combination of covariates the fitted values are wanted for.  If this is
+##' omitted, then the original data used to fit the model are used, extracted by
+##' \code{model.frame(object)}. 
+##'
 ##' @param type \code{"response"} for mean survival
 ##'
 ##' \code{"quantile"} for quantiles of the survival distribution specified by \code{p}
@@ -16,29 +23,39 @@
 ##'
 ##' @param p vector of quantiles to return when \code{type="quantile"}
 ##'
-##' @param tidy If \code{TRUE} then a tidyverse-friendly format is returned.  This is a tibble with one row per individual in \code{newdata}.  If multiple predictions are requested per individual (e.g. multiple quantiles) then list columns are returned... TODO finalize.    If \code{FALSE} then a data frame with one row per individual and prediction is returned. 
+##' @param tidy If \code{TRUE} then a tidyverse-friendly format is returned.  This is a tibble with one row per individual in \code{newdata}.  If multiple predictions are requested per individual (e.g. multiple quantiles) then list columns are returned.    If \code{FALSE} then a data frame with one row per individual and prediction is returned. 
 ##'
-##' Otherwise a data frame is returned... 
+##' @param return_covs If \code{FALSE} (the default) then covariates are removed from the returned data, to match the behaviour of standard predict functions such as \code{\link{predict.lm}}.  If \code{TRUE}, then columns for the covariates are included in the returned data, which may be helpful for post-processing the results.
 ##'
-##' TODO remove covariates?
-##'
-##' If \code{FALSE} then a simple data frame is returned 
-##' 
 ##' @seealso \code{\link{summary.flexsurvreg}}
 ##'
-##' @return TODO document format
+##' @return If \code{tidy=TRUE} then a tibble with one row per individual in \code{newdatata}.  If multiple quantities are predicted per individual, then list columns are returned, see, e.g. \url{https://r4ds.had.co.nz/many-models.html#list-columns-1}.   If \code{tidy=FALSE}, then a data frame with one row per individual and prediction is returned. 
 ##'
 ##' @importFrom tidyr unnest
+##' 
 ##' @importFrom tibble tibble
 ##'
-##' @examples ## TODO document extraction with tidyverse tools
+##' @examples ## TODO as well as just examples of running the functions, document extraction with tidyverse tools for the examples that return list columns. 
 ##'
 ##' \dontrun{
 ##' fitg <- flexsurvreg(formula = Surv(futime, fustat) ~ age, data = ovarian, dist="gengamma")
+##'
+##' ## Simplest prediction: mean or median, for covariates defined by original dataset
 ##' predict(fitg)
 ##' predict(fitg, type="quantile", p=0.5)
-##' library(tidyverse) # TODO sort out deps
+##' predict(fitg, return_covs=TRUE)
+##'
+##' ## Simple prediction for user-defined covariate values
+##' predict(fitg, newdata=list(age=c(40, 50, 60)))
+##' predict(fitg, type="quantile", p=0.5, newdata=list(age=c(40,50,60)))
+##'
+##' ## Predict multiple quantiles 
+##' require(tibble) 
+##' require(tidyr) # TODO declare dependencies in DESCRIPTION, decide if Imports or Suggests
 ##' predict(fitg, type="quantile", p=c(0.1, 0.9))
+##'
+##' ## Multiple quantiles and multiple covariate values
+##' 
 ##' }
 predict.flexsurvreg <- function(object,
                                 newdata,
@@ -46,7 +63,8 @@ predict.flexsurvreg <- function(object,
                                 interval="none",
                                 se.fit=FALSE,
                                 p = c(0.1, 0.9),
-                                tidy=TRUE
+                                tidy=TRUE,
+                                return_covs=FALSE
                                 )
 {
     if (missing(newdata))
@@ -64,7 +82,7 @@ predict.flexsurvreg <- function(object,
     }
     else res <- NULL
     rename <- function(x){
-        ## if depend on tidyverse stuff could use that for this
+        ## If depend on tidyverse stuff could use dplyr::rename. Should only add dependencies where necessary, though if we are making list columns, we could assume the user is willing to load tidyverse to deal with them.
         oldnames <- c("est", "se",       "lcl",       "ucl")
         newnames  <- c("pred","std_error","pred_lower","pred_upper")
         for (i in seq_along(x))
@@ -76,11 +94,15 @@ predict.flexsurvreg <- function(object,
         res2 <- vector(ncol(res[[1]]), mode = "list")
         for (i in seq_along(res2))
             res2[[i]] <- lapply(res, function(x)x[,i])
-        res2 <- do.call(tibble, res2)
+        res2 <- do.call(tibble, res2) # TODO This bit's currently broken
         names(res2) <- names(res[[1]])
         res <- if (tidy) res2 else tidyr::unnest(res2)
     } else {
         res <- rename(res)
+    }
+    if (!return_covs){
+        covnames <- attr(model.frame(object),"covnames.orig")
+        res[,covnames] <- NULL
     }
     res
 }
