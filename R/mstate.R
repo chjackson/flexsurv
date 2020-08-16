cecec354067b7fd7796e508e0f3e517e350804c0
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2020-08-16 16:37:01 +0100

    fix "number of items to replace is not a multiple of replacement length" for sim.fmsm

--- R/mstate.R
+++ R/mstate.R
@@ -827,78 +827,87 @@ form.basepars.tcovs <- function(x, transi, # index of allowed transition
 ##' @export
 sim.fmsm <- function(x, trans=NULL, t, newdata=NULL, start=1, M=10, tvar="trans", tcovs=NULL, tidy=FALSE, debug=FALSE){
     if (is.null(trans)) {
-        if (!is.null(attr(x, "trans"))) trans <- attr(x, "trans")
+        if (!is.null(attr(x, "trans"))) 
+            trans <- attr(x, "trans")
         else stop("`trans` not supplied and not found in `x`")
     }
-    if (length(t)==1) t <- rep(t, M)
-    else if (length(t)!=M) stop("length of t should be 1 or M=",M)
-    if (length(start)==1) start <- rep(start, M)
-    else if (length(start)!=M) stop("length of start should be 1 or M=",M)
-
-    basepars.all <- pars.fmsm(x=x, trans=trans, newdata=newdata, tvar=tvar)
-
-    nst <- nrow(trans)
-    ## TODO only need a max time if model is transient, else if absorbing, can allocate these up front
-    res.st <- cur.st <- start
-    res.t <- cur.t <- rep(0, M)
-    todo <- seq_len(M)
-    while (any(todo)){
-        if (debug) { cat("cur.t\n"); cat(cur.t); cat("\n") }
-        if (debug) { cat("cur.st\n"); cat(cur.st); cat("\n") }
-        if (debug) { cat("TODO\n"); cat(todo); cat("\n") }
+    if (length(t) == 1) 
+        t <- rep(t, M)
+    else if (length(t) != M) 
+        stop("length of t should be 1 or M=", M)
+    if (length(start) == 1) 
+        start <- rep(start, M)
+    else if (length(start) != M) 
+        stop("length of start should be 1 or M=", M)
+    basepars.all <- flexsurv:::pars.fmsm(x = x, trans = trans, newdata = newdata, 
+        tvar = tvar)
+       nst <- nrow(trans) # number of states
+    res.st <- cur.st <- start # current and final states
+     res.t <- cur.t  <- rep(0, M) # current and final transition times
+      todo <- seq_len(M) # indices of active individuals (in transient state)
+    # loop over active individuals and determine next time/state
+    while (any(todo)) {
+        # last observation carried forward
         cur.st.out <- cur.st[todo]
-        cur.t.out <- cur.t[todo]
-        done <- numeric()
-        for (i in unique(cur.st[todo])){            
-            if (i %in% transient(trans)) {
-                ## simulate next time and states for people whose current state is i
-                transi <- na.omit(trans[i,])
-                ni <- sum(cur.st[todo]==i)
-                t.trans1 <- matrix(0, nrow=ni, ncol=length(transi))
-                ## simulate times to all potential destination states
-                for (j in seq_along(transi)) {         
-                    xbase <- if (is.flexsurvlist(x)) x[[transi[j]]] else x
-                    if (length(tcovs)>0){
-                        basepars <- form.basepars.tcovs(x, transi[j], newdata, tcovs, cur.t.out)
-                    } else 
-                        basepars <- as.list(as.data.frame(basepars.all[[transi[j]]]))
-
-                    fncall <- c(list(n=ni), basepars, xbase$aux)
-                    if (is.null(xbase$dfns$r)) stop("No random sampling function found for this model")
-                    t.trans1[,j] <- do.call(xbase$dfns$r, fncall)
+         cur.t.out <- cur.t[todo]
+              done <- numeric()
+        # loop over originating states 
+        for (i in unique(cur.st[todo])) {
+            if (i %in% flexsurv:::transient(trans)) { # only need to do anything if transient
+                  transi <- na.omit(trans[i, ])
+                # determine indices of individuals who are still transient AND in state i
+                    inds <- which(cur.st[todo] == i)
+                    ni   <- length(inds)
+                t.trans1 <- matrix(0, nrow = ni, ncol = length(transi))
+                for (j in seq_along(transi)) { # simulate times for all competing events
+                    xbase <- if (flexsurv:::is.flexsurvlist(x)) 
+                      x[[transi[j]]]
+                    else x
+                    if (length(tcovs) > 0) {
+                      basepars <- flexsurv:::form.basepars.tcovs(x, transi[j], 
+                        newdata, tcovs, cur.t.out)
+                    }
+                    else basepars <- as.list(as.data.frame(basepars.all[[transi[j]]]))
+                    fncall <- c(list(n = ni), basepars, xbase$aux)
+                    if (is.null(xbase$dfns$r)) 
+                      stop("No random sampling function found for this model")
+                    t.trans1[, j] <- do.call(xbase$dfns$r, fncall)
                 }
-                if (debug) { print(t(t.trans1)) }
-                ## simulated next state is the one with minimum simulated time
-                mc <- max.col(-t.trans1)
-                if (debug) { cat("mc\n"); cat(mc); cat("\n") }
-                if (debug) { cat("transi\n"); cat(transi); cat("\n") }
-                if (debug) { cat("trans[i,]\n"); cat(trans[i,]); cat("\n") }
-                next.state <- match(transi[mc], trans[i,])
-                if (debug) { cat("next.state\n"); cat(next.state); cat("\n") }
-                next.time <- t.trans1[cbind(seq_along(next.state), mc)]
-                if (debug) { cat("next.time\n"); cat(next.time); cat("\n") }
-                inds <- which(cur.st[todo]==i)
-                cur.t.out[inds] <- cur.t.out[inds] + next.time
-                ## if final simulated state is greater than target time, censor at target time
-                cens <- cur.t.out[inds] > t[inds]
-                cur.t.out[inds][cens] <- t[inds][cens]               
+                # pick the earliest transition
+                        mc <- max.col(-t.trans1)
+                next.state <- match(transi[mc], trans[i, ])
+                 next.time <- t.trans1[cbind(seq_along(next.state), mc)]
+                
+                # increment the cumulative times and curtail to censoring time
+                # if needed
+                      cur.t.out[inds] <- cur.t.out[inds] + next.time
+                                 cens <- cur.t.out[inds] > t[inds]
+                cur.t.out[inds][cens] <- t[inds][cens]
+
+                # make sure censored observations don't change state, 
+                # otherwise use next.state
                 cur.st.out[inds][!cens] <- next.state[!cens]
+                
+                # remove censored from todo
                 done <- todo[inds][cens]
             }
         }
+        # update current status
         cur.st[todo] <- cur.st.out
-        cur.t[todo] <- cur.t.out
+         cur.t[todo] <- cur.t.out
+        # append as column to res
         res.st <- cbind(res.st, cur.st)
-        res.t <- cbind(res.t, cur.t)
-        done <- union(done, which(cur.st %in% absorbing(trans)))
+         res.t <- cbind(res.t, cur.t)
+        # check for arrival in absorbing state
+        done <- union(done, which(cur.st %in% flexsurv:::absorbing(trans)))
         todo <- setdiff(todo, done)
-        if (debug) { cat("\n") }
     }
-    res <- list(st=unname(res.st), t=unname(res.t))
+    res <- list(st = unname(res.st), t = unname(res.t))
     attr(res, "trans") <- trans
     attr(res, "statenames") <- attr(x, "statenames")
-    if (tidy) res <- simfs_bytrans(res)
-    res  # TODO set S3 class and adapt methods 
+    if (tidy) 
+        res <- simfs_bytrans(res)
+    res # TODO set S3 class and adapt methods 
 }
 
 
