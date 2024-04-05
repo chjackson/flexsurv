87b7e3382bf13b03a3c6b7929d2f44ea4611b3f8
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2024-04-05 14:26:53 +0100

    Remove old URL

--- R/survsplinek.R
+++ R/survsplinek.R
@@ -4,7 +4,7 @@
 ##' cumulative hazard, mean and restricted mean functions for the Royston/Parmar
 ##' spline model, with one argument per parameter.   For the equivalent functions with all parameters collected together in a single argument, see \code{\link{Survspline}}.
 ##'
-##' These functions go up to 7 spline knots, or 9 parameters.  If you'd like higher-dimension versions, just submit an issue at \url{https://github.com/chjackson/flexsurv-dev/issues}.
+##' These functions go up to 7 spline knots, or 9 parameters.
 ##'
 ##' @aliases dsurvspline0 dsurvspline1 dsurvspline2 dsurvspline3 dsurvspline4 dsurvspline5 dsurvspline6 dsurvspline7     psurvspline0 psurvspline1 psurvspline2 psurvspline3 psurvspline4 psurvspline5 psurvspline6 psurvspline7     qsurvspline0 qsurvspline1 qsurvspline2 qsurvspline3 qsurvspline4 qsurvspline5 qsurvspline6 qsurvspline7   rsurvspline0 rsurvspline1 rsurvspline2 rsurvspline3 rsurvspline4 rsurvspline5 rsurvspline6 rsurvspline7  hsurvspline0 hsurvspline1 hsurvspline2 hsurvspline3 hsurvspline4 hsurvspline5 hsurvspline6 hsurvspline7  Hsurvspline0 Hsurvspline1 Hsurvspline2 Hsurvspline3 Hsurvspline4 Hsurvspline5 Hsurvspline6 Hsurvspline7  mean_survspline0 mean_survspline1 mean_survspline2 mean_survspline3 mean_survspline4 mean_survspline5 mean_survspline6 mean_survspline7    rmst_survspline0 rmst_survspline1 rmst_survspline2 rmst_survspline3 rmst_survspline4 rmst_survspline5 rmst_survspline6 rmst_survspline7      
 ##'
